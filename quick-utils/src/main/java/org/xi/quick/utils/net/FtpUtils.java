965953b6f3c1b3ccfcdaf23cec719cefe8a47d9c
xi <rarexixi@gmail.com>
2018-08-16 15:20:58 +0800

    FTP 工具类修改

--- quick-utils/src/main/java/org/xi/quick/utils/net/FtpUtils.java
+++ quick-utils/src/main/java/org/xi/quick/utils/net/FtpUtils.java
@@ -1,61 +1,136 @@
 package org.xi.quick.utils.net;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.net.ftp.*;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.function.Function;
 
 public class FtpUtils {
 
-    private static FTPClient ftpClient = new FTPClient();
+    String host;
+    int port;
+    String username;
+    String password;
+    boolean passiveMode;
 
-    public static void init(String host, int port, String username, String password, boolean passiveMode) throws IOException {
+    public FtpUtils(String host, String username, String password) throws IOException {
+        this(host, 21, username, password, false);
+    }
 
+    public FtpUtils(String host, String username, String password, boolean passiveMode) throws IOException {
+        this(host, 21, username, password, passiveMode);
+    }
+
+    public FtpUtils(String host, int port, String username, String password) throws IOException {
+        this(host, port, username, password, false);
+    }
+
+    public FtpUtils(String host, int port, String username, String password, boolean passiveMode) {
+        this.host = host;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+        this.passiveMode = passiveMode;
+    }
+
+    private FTPClient getFtpClient() throws IOException {
+        FTPClient ftpClient = new FTPClient();
         ftpClient.connect(host, port);
         ftpClient.login(username, password);
 
+        // 设置上传为二进制文件
         ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
 
         // 设置被动模式
         if (passiveMode) ftpClient.enterLocalPassiveMode();
+
+        return ftpClient;
     }
 
-    public static void upload(String remoteDir, String fileName, InputStream inputStream) throws IOException {
+    public void upload(String remoteDir, String fileName, InputStream inputStream) {
+        execute(ftpClient -> {
+            try {
+                if (remoteDir.startsWith("/")) ftpClient.changeWorkingDirectory("/");
 
-        ftpClient.makeDirectory(remoteDir);
-        ftpClient.changeWorkingDirectory(remoteDir);
-        ftpClient.storeFile(fileName, inputStream);
-    }
-
-    public static void download(String remoteDir, String fileName, OutputStream outputStream) throws IOException {
-
-        ftpClient.changeWorkingDirectory(remoteDir);
-        // 得到FTP当前目录下所有文件
-        FTPFile[] ftpFiles = ftpClient.listFiles();
-        // 循环遍历
-        for (FTPFile ftpFile : ftpFiles) {
-            // 找到需要下载的文件
-            if (ftpFile.getName().equals(fileName)) {
-                ftpClient.retrieveFile(fileName, outputStream);
+                for (String folder : remoteDir.split("/")) {
+                    if (StringUtils.isEmpty(folder)) continue;
+                    if (!ftpClient.changeWorkingDirectory(folder)) {
+                        ftpClient.makeDirectory(folder);
+                    }
+                    ftpClient.changeWorkingDirectory(folder);
+                }
+                ftpClient.storeFile(fileName, inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }
+            return null;
+        });
     }
 
-    public static FTPFile[] listFiles(String remoteDir) throws IOException {
-
-        FTPFile[] files = ftpClient.listFiles(remoteDir);
-        return files;
+    public void download(String remoteDir, String fileName, OutputStream outputStream) {
+        execute(ftpClient -> {
+            try {
+                ftpClient.changeWorkingDirectory(remoteDir);
+                // 得到FTP当前目录下所有文件
+                FTPFile[] ftpFiles = ftpClient.listFiles();
+                // 循环遍历
+                for (FTPFile ftpFile : ftpFiles) {
+                    // 找到需要下载的文件
+                    if (ftpFile.getName().equals(fileName)) {
+                        ftpClient.retrieveFile(fileName, outputStream);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
     }
 
-    public static void close() {
+    public FTPFile[] listFiles(String remoteDir) {
+        return execute(ftpClient -> {
+            try {
+                return ftpClient.listFiles(remoteDir);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
+    }
+
+    /**
+     * 关闭连接
+     */
+    public <T> T execute(Function<FTPClient, T> function) {
+        FTPClient ftpClient = null;
         try {
-            if (ftpClient != null) {
-                ftpClient.logout();
-                ftpClient.disconnect();
+            ftpClient = getFtpClient();
+            if (function != null) {
+                return function.apply(ftpClient);
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (ftpClient != null) {
+                try {
+                    if (ftpClient != null) {
+                        ftpClient.logout();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (ftpClient.isConnected())
+                            ftpClient.disconnect();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
+        return null;
     }
 }
--- quick-utils/src/test/java/org/xi/quick/utils/net/FtpUtilsTests.java
+++ quick-utils/src/test/java/org/xi/quick/utils/net/FtpUtilsTests.java
@@ -3,51 +3,50 @@ package org.xi.quick.utils.net;
 import org.apache.commons.net.ftp.*;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class FtpUtilsTests {
 
     @Test
     public void listFilesTest() throws IOException {
 
-        FtpUtils.init("192.168.1.63", 21, "ftp", "123456", false);
+        FtpUtils ftpUtils = new FtpUtils("192.168.1.63", 21, "ftp", "123456");
 
         // 循环遍历
-        for (FTPFile ftpFile : FtpUtils.listFiles("/")) {
+        for (FTPFile ftpFile : ftpUtils.listFiles("/")) {
             System.out.println(ftpFile.getName());
         }
-        FtpUtils.close();
     }
 
     @Test
     public void uploadTest() throws IOException {
 
-        FtpUtils.init("192.168.1.63", 21, "ftp", "123456", false);
+        FtpUtils ftpUtils = new FtpUtils("192.168.1.63", 21, "ftp", "123456");
 
         File file = new File("/Users/xi/Pictures/5940c8e0959cb.jpg");
         try (FileInputStream inputStream = new FileInputStream(file)) {
-            FtpUtils.upload("/pic/", file.getName(), inputStream);
+            ftpUtils.upload("/bjev/201808/16/", file.getName(), inputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        FtpUtils.close();
+        listFilesTest();
     }
 
     @Test
     public void downloadTest() throws IOException {
 
-        FtpUtils.init("192.168.1.63", 21, "ftp", "123456", false);
+        FtpUtils ftpUtils = new FtpUtils("192.168.1.63", 21, "ftp", "123456");
 
         // 循环遍历
-        for (FTPFile ftpFile : FtpUtils.listFiles("/")) {
+        for (FTPFile ftpFile : ftpUtils.listFiles("/")) {
             if (ftpFile.isDirectory()) continue;
             File file = new File("/Users/xi/tmp/test/" + ftpFile.getName());
             try (FileOutputStream outputStream = new FileOutputStream(file)) {
-                FtpUtils.download("/", file.getName(), outputStream);
+                ftpUtils.download("/", file.getName(), outputStream);
                 outputStream.flush();
             }
         }
-        FtpUtils.close();
     }
 }
